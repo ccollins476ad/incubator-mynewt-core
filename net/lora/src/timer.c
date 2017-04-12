4913a6cf7ad0f304ac358513233db124a2d4f0ca
Christopher Collins <ccollins@apache.org>
2017-04-12 13:51:49 -0700

    Fill in TimerGetElapsedTime()+TimerGetFutureTime()

--- net/lora/src/timer.c
+++ net/lora/src/timer.c
@@ -1,6 +1,17 @@
+#include "os/os.h"
 #include "lora/timer.h"
 #include "lora/utilities.h"
 
-uint32_t TimerGetElapsedTime(uint32_t savedTime) { return 0; }
-uint32_t TimerGetFutureTime(uint32_t eventInFuture) { return 0; }
+uint32_t
+TimerGetElapsedTime(uint32_t savedTime)
+{ 
+    return savedTime - os_cputime_get32();
+}
+
+uint32_t
+TimerGetFutureTime(uint32_t eventInFuture)
+{
+    return os_cputime_get32() + eventInFuture;
+}
+
 void TimerLowPowerHandler(void) { }
