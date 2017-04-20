d0f3275b5de85bf17835fc14606fcdc81ca52531
Christopher Collins <ccollins@apache.org>
2017-04-19 17:50:25 -0700

    more

--- hw/drivers/loramac-node/board/nrf52/include/board/board.h
+++ hw/drivers/loramac-node/board/nrf52/include/board/board.h
@@ -27,56 +27,20 @@ Maintainer: Miguel Luis and Gregory Cristian
  * Board MCU pins definitions
  */
 
-#define RADIO_RESET                                 SX1276_NRESET
+#define RADIO_RESET                         SX1276_NRESET
 
-#define RADIO_NSS                                   MYNEWT_VAL(SPI_0_MASTER_SS_PIN)
+#define RADIO_NSS                           MYNEWT_VAL(SPI_0_MASTER_SS_PIN)
 
-#define RADIO_DIO_0                                 SX1276_DIO0
-#define RADIO_DIO_1                                 SX1276_DIO1
-#define RADIO_DIO_2                                 SX1276_DIO2
-#define RADIO_DIO_3                                 SX1276_DIO3
-#define RADIO_DIO_4                                 SX1276_DIO4
-#define RADIO_DIO_5                                 SX1276_DIO5
+#define RADIO_DIO_0                         SX1276_DIO0
+#define RADIO_DIO_1                         SX1276_DIO1
+#define RADIO_DIO_2                         SX1276_DIO2
+#define RADIO_DIO_3                         SX1276_DIO3
+#define RADIO_DIO_4                         SX1276_DIO4
+#define RADIO_DIO_5                         SX1276_DIO5
 
-#define RADIO_ANT_SWITCH_HF                         SX1276_ANT_HF_CTRL
-//#define RADIO_ANT_SWITCH_LF                         1 //PA_1
+#define RADIO_ANT_SWITCH_HF                 SX1276_ANT_HF_CTRL
 
-//#define OSC_LSE_IN                                  1 //PC_14
-//#define OSC_LSE_OUT                                 1 //PC_15
-
-//#define OSC_HSE_IN                                  1 //PH_0
-//#define OSC_HSE_OUT                                 1 //PH_1
-
-//#define USB_DM                                      1 //PA_11
-//#define USB_DP                                      1 //PA_12
-
-//#define I2C_SCL                                     1 //PB_6
-//#define I2C_SDA                                     1 //PB_7
-
-//#define BOOT_1                                      1 //PB_2
-
-//#define GPS_PPS                                     1 //PB_1
-//#define UART_TX                                     1 //PA_9
-//#define UART_RX                                     1 //PA_10
-
-//#define DC_DC_EN                                    1 //PB_8
-//#define BAT_LEVEL_PIN                               1 //PB_0
-//#define BAT_LEVEL_CHANNEL                           1 //ADC_CHANNEL_8
-
-//#define WKUP1                                       1 //PA_8
-//#define USB_ON                                      1 //PA_2
-
-#define RF_RXTX                                     SX1276_RXTX
-
-//#define SWDIO                                       1 //PA_13
-//#define SWCLK                                       1 //PA_14
-
-//#define TEST_POINT1                                 1 //PB_12
-//#define TEST_POINT2                                 1 //PB_13
-//#define TEST_POINT3                                 1 //PB_14
-//#define TEST_POINT4                                 1 //PB_15
-
-//#define PIN_NC                                      1 //PB_5
+#define RF_RXTX                             SX1276_RXTX
 
 /*!
  * Possible power sources
--- net/loramac-node/README.md
+++ net/loramac-node/README.md
@@ -1,3 +1,4 @@
+<!--
 #
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
@@ -6,26 +7,22 @@
 # to you under the Apache License, Version 2.0 (the
 # "License"); you may not use this file except in compliance
 # with the License.  You may obtain a copy of the License at
-# 
-#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing,
 # software distributed under the License is distributed on an
 # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
+#  KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
 #
+-->
 
-pkg.name: hw/drivers/loramac-node/radio/sx1276
-pkg.description: LoRaMac-node radio driver for the SX1276 modem.
-pkg.author: "Semtech"
-pkg.homepage: "http://mynewt.apache.org/"
-pkg.keywords:
-    - lora
-pkg.deps:
-    - "@apache-mynewt-core/kernel/os"
-    - "@apache-mynewt-core/net/loramac-node"
+# loramac-node
 
-pkg.apis:
-    - loramac_radio
+This is a Mynewt port of the Semtech LoRaWAN endpoint stack.  This package depends on two drivers:
+    * loramac-net radio - Facilitates communication with a particular type of LoRa modem.  This package exports the Radio_s definition containing the function pointers required by this stack.
+    * loramac-net board - Contains MCU or BSP specific definition needed for handling interrupts triggered by the LoRa radio.
+
+Sample drivers of both types can be found at: @apache-mynewt-core/hw/drivers/loramc-node.
