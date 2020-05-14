a1d7e86c2ee62aadeab7989fe122e86a5c329cf9
Christopher Collins <ccollins@apache.org>
2020-05-14 12:25:31 -0700

    extract common tcp reassembly

---
+++ net/oic/include/oic/port/mynewt/tcp.h
@@ -0,0 +1,50 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#ifndef H_MYNEWT_TCP_
+#define H_MYNEWT_TCP_
+
+#include "os/mynewt.h"
+#include "oic/oc_ri.h"
+#include "oic/oc_ri_const.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef bool oc_tcp_frag_belongs_fn(const struct os_mbuf *frag,
+                                    void *arg);
+
+typedef void oc_tcp_fill_endpoint(struct os_mbuf *om, void *arg);
+
+struct oc_tcp_reassembler {
+    STAILQ_HEAD(, os_mbuf_pkthdr) pkt_q;
+    oc_tcp_frag_belongs_fn *frag_belongs;
+    oc_tcp_fill_endpoint *fill_endpoint;
+    int endpoint_size;
+};
+
+int oc_tcp_reass(struct oc_tcp_reassembler *reassembler, struct os_mbuf *om1,
+                 void *arg, struct os_mbuf **out_pkt);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ net/oic/src/port/mynewt/tcp.c
@@ -0,0 +1,62 @@
+#include "os/mynewt.h"
+#include "oic/port/mynewt/tcp.h"
+#include "oic/messaging/coap/coap.h"
+
+int
+oc_tcp_reass(struct oc_tcp_reassembler *r, struct os_mbuf *om1, void *arg,
+             struct os_mbuf **out_pkt)
+{
+    struct os_mbuf_pkthdr *pkt1;
+    struct os_mbuf_pkthdr *pkt2;
+    struct os_mbuf *om2;
+    uint8_t hdr[sizeof (struct coap_tcp_hdr32)];
+
+    *out_pkt = NULL;
+
+    pkt1 = OS_MBUF_PKTHDR(om1);
+    assert(pkt1);
+
+    /* Find the packet that this fragment belongs to, if any. */
+    STAILQ_FOREACH(pkt2, &r->pkt_q, omp_next) {
+        om2 = OS_MBUF_PKTHDR_TO_MBUF(pkt2);
+        if (r->frag_belongs(om2, arg)) {
+            /* Data from same connection.  Append. */
+            os_mbuf_concat(om2, om1);
+            os_mbuf_copydata(om2, 0, sizeof(hdr), hdr);
+
+            if (coap_tcp_msg_size(hdr, sizeof(hdr)) <= pkt2->omp_len) {
+                /* Packet complete. */
+                STAILQ_REMOVE(&r->pkt_q, pkt2, os_mbuf_pkthdr, omp_next);
+                *out_pkt = om2;
+            }
+
+            return 0;
+        }
+    }
+
+    /*
+     * New frame, need to add endpoint to the front.  Check if there is enough
+     * space available. If not, allocate a new pkthdr.
+     */
+    if (OS_MBUF_USRHDR_LEN(om1) < r->endpoint_size) {
+        om2 = os_msys_get_pkthdr(0, r->endpoint_size);
+        if (!om2) {
+            return SYS_ENOMEM;
+        }
+        OS_MBUF_PKTHDR(om2)->omp_len = pkt1->omp_len;
+        SLIST_NEXT(om2, om_next) = om1;
+    } else {
+        om2 = om1;
+    }
+
+    r->fill_endpoint(om2, arg);
+    pkt2 = OS_MBUF_PKTHDR(om2);
+
+    os_mbuf_copydata(om2, 0, sizeof(hdr), hdr);
+    if (coap_tcp_msg_size(hdr, sizeof(hdr)) > pkt2->omp_len) {
+        STAILQ_INSERT_TAIL(&r->pkt_q, pkt2, omp_next);
+    } else {
+        *out_pkt = om2;
+    }
+    return 0;
+}
